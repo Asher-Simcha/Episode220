384edb91b893044551da43a24ef374a61eec5f82
twit-KnowHow <ashersimcha42@gmail.com>
2017-03-24 21:43:34 -0700

    Add files via upload
    We build our own Pan-Tilt-Zoom with an Arduino and a GoPro!
    Stepduino 3 Assembly
    
    Project Description
    * We're taking the pieces from "Stepduino 2" (Episode 211) and turning it into a functioning PTZ mount
    * This is NOT a finished project, but a proof-of-concept
    
    Parts and Tools
    1. 2 x 28BYJ-48 Motor + ULN2003 Driver $2.08 http://www.ebay.com/itm/191687898081?_trksid=p2057872.m2749.l2649&ssPage...
    2. 5V UBEC $2.68 http://www.ebay.com/itm/Hobbywing-3A-RC-UBEC-5V-6V-Switch-Mode-Lowest-RF...
    3. Arduino
    4. Jumpers
    5. 3D Printer
    6. Power Source
    
    3D Printed Parts:
    1. Base
    2. Bottom Shelf for Stepper 1
    3. Shaft Adapters
    4. Top Shelf for Stepper 2
    5. GoPro Mount 
    
    Steps:
    1. Install Stepper 1 into the bottom shelf using M3 screws and nuts
    2. Connect Stepper 1 to Driver Board 1
    3. Connect Driver Board 1 to 4 pins on the Arduino
    4. Install Stepper 2 into the top shelf
    5. Connect Stepper 2 to Driver Board 2
    6. Attach Top Shelf to the shaft adapter for the lower stepper mot

---
+++ KH220_Stepduino3/KH220_Stepduino3.ino
@@ -0,0 +1,154 @@
+// Each driver board will have pins labeled "IN1", "IN2", "IN3", & "IN4"
+// These are the pins that need to be connected to digital out pins on your Arduino.
+// The variables below give you an easy way to assign which pins on the driver board are
+// connected to corresponding pins on the Arduino.
+
+// These are for stepper #1 (the pan)
+int S1_IN1 = 4;    
+int S1_IN2 = 5;
+int S1_IN3 = 6;
+int S1_IN4 = 7;
+
+//These are for stepper #2 (the tilt)
+int S2_IN1 = 8;    
+int S2_IN2 = 9;
+int S2_IN3 = 10;
+int S2_IN4 = 11;
+
+
+int Speed = 1;        // This controls how long we wait between pulses (In seconds)
+int Distance = 0;        // Creating an integer variable named "Distance" that counts the number of steps we've made
+int FullRotation = 4096; // How many steps are in a full revolution of the motor shaft?
+int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
+                          // This array contains bimary values that will be pushed to the outpur pins to properly pulse the stepper
+
+void setup() {
+  
+// Set mode on the pins for stepper #1  
+  pinMode(S1_IN1, OUTPUT);
+  pinMode(S1_IN2, OUTPUT);
+  pinMode(S1_IN3, OUTPUT);
+  pinMode(S1_IN4, OUTPUT);
+
+// And now lets do the same for stepper #2
+  pinMode(S2_IN1, OUTPUT);
+  pinMode(S2_IN2, OUTPUT);
+  pinMode(S2_IN3, OUTPUT);
+  pinMode(S2_IN4, OUTPUT);
+
+// And open up a serial port so we can debug our program
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+        // We only want to do something when we receive data
+        // This if statement puts us into pause until the arduino receives data from the serial port
+        
+        if (Serial.available() > 0) 
+          {
+                int incomingByte = Serial.read(); // Read the incoming data
+
+                Serial.print("I received: ");
+                Serial.println(incomingByte);
+
+                // IF the arduino has received data, then evaluate the following four IF statements
+
+                // If the arduino has recieved ASCII "97" - ("a")
+                if (incomingByte == 97)
+                {
+                  for (int x=0; x < 3; x++)
+                  {
+                    CCW();
+                  }
+                }
+                
+                // If the arduino has received ASCII "100" - ("d")
+                if (incomingByte == 100)
+                {
+                  for (int x=0; x < 3; x++)
+                  {
+                    CW();
+                  }
+                }
+
+                // If the arduino has received ASCII "119" - ("w")
+                if (incomingByte == 119)
+                {
+                  for (int x=0; x < 3; x++)
+                  {
+                    UP();
+                  }
+                }
+        
+
+                // If the arduino has received ASCII "115" - ("s")
+                if (incomingByte == 115)
+                {
+                  for (int x=0; x < 3; x++)
+                  {
+                    DOWN();
+                  }
+                }
+
+        
+        }
+
+
+}
+
+
+void CW()
+{
+  for(int stepper = 7; stepper >= 0; stepper--)
+  {
+    MoveStep1(stepper);
+    delay(Speed);
+  }
+}
+
+void CCW()
+{
+  for(int stepper = 0; stepper < 8; stepper++)
+  {
+    MoveStep1(stepper);
+    delay(Speed);
+  }
+}
+
+void UP()
+{
+  for(int stepper = 7; stepper >= 0; stepper--)
+  {
+    MoveStep2(stepper);
+    delay(Speed);
+  }  
+}
+
+void DOWN()
+{
+  for(int stepper = 0; stepper < 8; stepper++)
+  {
+    MoveStep2(stepper);
+    delay(Speed);
+  }  
+}
+
+// This function will move stepper #1 one step in either direction (depending on which function called this function)
+void MoveStep1(int eighth)
+{
+  digitalWrite(S1_IN1, bitRead(lookup[eighth], 0));
+  digitalWrite(S1_IN2, bitRead(lookup[eighth], 1));
+  digitalWrite(S1_IN3, bitRead(lookup[eighth], 2));
+  digitalWrite(S1_IN4, bitRead(lookup[eighth], 3));
+}
+
+// This function will move stepper #2 one step in either direction (depending on which function called this function)
+void MoveStep2(int eighth)
+{
+  digitalWrite(S2_IN1, bitRead(lookup[eighth], 0));
+  digitalWrite(S2_IN2, bitRead(lookup[eighth], 1));
+  digitalWrite(S2_IN3, bitRead(lookup[eighth], 2));
+  digitalWrite(S2_IN4, bitRead(lookup[eighth], 3));
+}
